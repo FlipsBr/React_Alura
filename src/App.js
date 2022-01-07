f654a66e0b289f09a3615091ccb00ba441d15320
Felipe <fpires.92@gmail.com>
2022-01-07 18:30:42 -0300

    terminado parte 2 do curso + modificações minhas

--- src/App.js
+++ src/App.js
@@ -1,34 +1,62 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
 import "./assets/index.css";
+import Categorias from "./dados/Categorias";
+import ArrayDeNotas from "./dados/Notas";
 class App extends Component {
   constructor() {
     super();
-    this.notas = [];
-    this.state = {
-      notas: [],
-    };
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
+    /* Sem alterar o State, o react não re-renderiza a virtual DOM. */
   }
-  criarNota(texto, titulo) {
-    const novaNota = { titulo, texto };
-    const novasNotas = [...this.state.notas, novaNota];
-    console.log(`uma nova nota foi criada.2 ${titulo}, ${texto}`);
-    const novoEstado = {
-      notas: novasNotas,
-    };
-    this.setState(novoEstado);
-  }
-
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
-        <ListaDeNotas notas={this.state.notas} />
+        <FormularioCadastro
+          categorias={this.categorias}
+          criarNota={this.notas.criarNota.bind(this.notas)}
+        />
+        <main className="conteudo-principal">
+          <ListaDeCategorias
+            categorias={this.categorias}
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(
+              this.categorias
+            )}
+          />
+          <ListaDeNotas
+            notas={this.notas}
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
+          />
+        </main>
       </section>
     );
   }
 }
 
 export default App;
+
+/*criarNota(texto, titulo, categoria) {
+    const novaNota = { titulo, texto, categoria };
+    console.log(`nova categoria criada: ${titulo}, ${texto},${categoria}`);
+    const novasNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novasNotas,
+    };
+    this.setState(novoEstado);
+  }*/
+
+/*adicionarCategoria(nomeCategoria) {
+    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
+    const newState = { ...this.state, categorias: novoArrayCategorias };
+    this.setState(newState);
+  }*/
+
+/*apagarNota(indice) {
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(indice, 1);
+    this.setState({ notas: arrayNotas });
+  }*/
--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -2,13 +2,32 @@ import React, { Component } from "react";
 import CardNota from "../CardNota";
 import "./estilo.css";
 class ListaDeNotas extends Component {
+  constructor() {
+    super();
+    this.state = { notas: [] };
+  }
+  componentDidMount() {
+    this.props.notas.inscrever(this._novasNotas.bind(this));
+  }
+  _novasNotas(notas) {
+    this.setState({ ...this.state, notas });
+  }
+  /* Stateless Component, i.e.: Ele não gerencia nenhum estado,
+   apesar de renderizar estados vindos de outros elementos statefull */
   render() {
     return (
+      /* Notas é o conjunto de notas, nota é uma em particular. */
       <ul className="lista-notas">
-        {this.props.notas.map((nota, index) => {
+        {this.state.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota titulo={nota.titulo} texto={nota.texto} />
+              <CardNota
+                indice={index}
+                apagarNota={this.props.apagarNota}
+                titulo={nota.titulo}
+                texto={nota.texto}
+                categoria={nota.categoria}
+              />
             </li>
           );
         })}
